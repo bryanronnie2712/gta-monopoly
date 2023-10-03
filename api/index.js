faf808bb21a697b1d57a014291a4508f0cc39847
TheGradsPlace <70072321+bryanronnie2712@users.noreply.github.com>
2023-10-03 18:56:46 +0000

    Room creation and player joining

--- api/index.js
+++ api/index.js
@@ -13,17 +13,83 @@ const io = new Server(server, {
   },
 });
 
+let currentGames = new Object();
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
-
-  //   Joining a room
-  socket.on("join_room", (data) => {
-    socket.join(data);
+  // Create a new room
+  socket.on("createRoom", (data) => {
+    // data = {playerName: String, playerId: String}
+    const randomRoomId = Math.floor(Math.random() * 10000);
+    // Update player details - 1st player, so no conditions needed
+    currentGames[randomRoomId] = {
+      roomId: randomRoomId,
+      players: [
+        {
+          playerNumber: 0,
+          playerId: data.playerId,
+          name: data.playerName,
+          pos: 0,
+          money: 1500,
+          assets: [],
+        },
+      ],
+    };
+    console.log("currentGames ==>", currentGames, data);
+    // Creating and Joining this room
+    socket.join(randomRoomId);
+    // Reply to joiner
+    socket.emit("createRoomStatus", {
+      updPlayerDetails: currentGames[randomRoomId].players,
+      roomId: randomRoomId,
+      playerNumber: 0,
+      msg: "created",
+    });
   });
 
-  //   Update player details after each turn
-  socket.on("send_msg", (data) => {
-    socket.to(data.room).emit("recieve", data);
+  // Joining a room
+  socket.on("joinRoom", (data) => {
+    // data = {roomId: String, playerName: String, playerId: String}
+    console.log("joinRoom", data);
+    const roomIdAvailable = Object.keys(currentGames).includes(data.roomId);
+    const roomSize = currentGames[data.roomId]?.players?.length;
+    const roomNotFull = roomSize < 4;
+    if (roomIdAvailable) {
+      if (roomNotFull) {
+        // Update player details
+        currentGames[data.roomId].players.push({
+          playerNumber: roomSize,
+          playerId: data.playerId,
+          name: data.playerName,
+          pos: 0,
+          money: 1500,
+          assets: [],
+        });
+        // Joining this room
+        socket.join(data.roomId);
+        // Reply to joiner
+        socket.emit("joinRoomStatus", {
+          updPlayerDetails: currentGames[data.roomId].players,
+          playerNumber: roomSize,
+          roomId: data.roomId,
+          msg: "joined",
+        });
+        // Broadcast to roomId
+        socket.broadcast.emit("aNewPlayerHasJoined", {
+          updPlayerDetails: currentGames[data.roomId].players,
+          newPlayerName: data.playerName,
+          msg: "joined",
+        });
+      } else {
+        // Room is full
+        // Disconnect from socket
+        socket.disconnect(true);
+        // Reply to joiner
+        socket.emit("joinRoomStatus", {
+          msg: "full",
+        });
+      }
+    } else {
+      console.log("Room Id does not exist");
+    }
   });
 });
 
